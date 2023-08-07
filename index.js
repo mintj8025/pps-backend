d3003246ddd50fc396a154fcd7c4c6b53b7a94dd
mintj8025 <83228991+mintj8025@users.noreply.github.com>
2023-08-07 19:11:46 +0700

    if HN length error

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/register', jsonParser , function (req, res, next) {
                   if(err) {
                     res.json({status: 'error', message: 'err'})
                     return
-                  }else if(req.body.patient_HN.length < 8 || req.body.patient_HN.length > 8){
+                  }if(req.body.patient_HN.length < 8 || req.body.patient_HN.length > 8){
                     res.json({status: 'error' , message: 'HN number must be at 8 int!'})
                     return
                   }else {
