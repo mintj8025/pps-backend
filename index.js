eaaeb1220a1dae7b999a20a961d5e932a812c902
mintj8025 <83228991+mintj8025@users.noreply.github.com>
2023-08-07 19:12:59 +0700

    status HN error

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.post('/register', jsonParser , function (req, res, next) {
                     res.json({status: 'error', message: 'err'})
                     return
                   }if(req.body.patient_HN.length < 8 || req.body.patient_HN.length > 8){
-                    res.json({status: 'error' , message: 'HN number must be at 8 int!'})
+                    res.json({status: 'HN error' , message: 'HN number must be at 8 int!'})
                     return
                   }else {
                   res.json({status: 'ok'})
