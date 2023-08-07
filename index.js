0993ef4c8b377827c2a64d2e59845a9cd3037045
mintj8025 <83228991+mintj8025@users.noreply.github.com>
2023-08-07 18:22:44 +0700

    register patient length error

--- index.js
+++ index.js
@@ -71,9 +71,12 @@ app.post('/register', jsonParser , function (req, res, next) {
                   if(err) {
                     res.json({status: 'error', message: 'err'})
                     return
-                  }
+                  }else if(req.body.patient_HN.length < 8 || req.body.patient_HN.length > 8){
+                    res.json({status: 'length error' , message: 'HN number must be at 8 int!'})
+                    return
+                  }else {
                   res.json({status: 'ok'})
-                }
+                }}
                 );
             })
 
